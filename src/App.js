89bf044aa1043557c46c33381424cea2e4c7f9b2
sudhir kumar <65527808+titanSlayer12@users.noreply.github.com>
2021-10-01 20:28:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Root from './components/Root'
+
+function App() {
+          return (
+                    <>
+
+                              <h1>Welcome to react app</h1>
+                              <Root />
+                    </>
+          )
+}
+
+export default App
